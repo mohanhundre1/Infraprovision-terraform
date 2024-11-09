20f1e49e933a5dd3067d8c68fcad6b031272ab64
mohanhundre1 <mohanhundre1@gmail.com>
2024-11-09 18:04:02 +0530

    terraform version add

--- global/provider.tf
+++ global/provider.tf
@@ -1,4 +1,12 @@
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.75.0"  # Use an appropriate version for your use case
+    }
+  }
+}
 
 provider "aws" {
   region = var.aws_region
