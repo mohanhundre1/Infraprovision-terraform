859bdc2396557ac97da13a5388bc877d14e84d1f
Mohan Hundre <mohanhundre1@gmail.com>
2024-11-26 19:23:49 +0530

    updated the region and ami-id

--- test/main.tf
+++ test/main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "eu-west-1"  # Set your desired AWS region
+  region = "us-east-1"  # Set your desired AWS region
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0d64bb532e0502c46"  # Replace with a valid AMI ID for your region
+  ami           = "ami-0866a3c8686eaeeba"  # Replace with a valid AMI ID for your region
   instance_type = "t2.micro"      # Choose your instance type
 
   tags = {
